93b01a2bf555cad31c24db4d4fdef39b484753da
Artem Karnaukhov <122673491+artmare@users.noreply.github.com>
2026-04-14 12:44:55 +0200

    Add lesson example scripts for Lessons 3-5
    Add three new example scripts: Lesson_3/main.py prints a simple gold/silver message (Ukrainian). Lesson_4/main.py contains several commented conditional examples and an active input-based check that allows entry only if height >= 110 cm and age >= 10. Lesson_5/main.py imports time and prints a "Spaceship is ready to launch" message five times with a 1s pause between prints.

---
+++ Lesson_4/main.py
@@ -0,0 +1,85 @@
+
+
+
+# money = 0
+ 
+# if money:
+#     print(f"You have {money} dollars on your account")
+# else:
+#     print("You don't have money on your account")
+
+    
+
+# result = 5.5
+
+# if result:
+#     print(f"Your result is {result}")
+# else:
+#     print("You don't have result yet")
+    
+
+
+
+
+# user = input("Enter your name: \n")
+
+# if user:
+#     print(f"Hello, {user}!")
+# else:
+#     print("Hello, Anonym")
+    
+    
+
+
+
+
+# a = 15
+# b = 155
+
+# c = a if a > b else b
+
+# print(c)
+
+
+
+
+
+
+
+# x = int(input("Enter a number: \n"))
+
+# y = int(input("Enter another number: \n"))
+# if y == 0:
+#     print("You entered zero")
+#     y = int(input("Enter another number: \n"))
+    
+    
+# result = x/y 
+# print(f"Result of division is {result}")
+
+
+
+
+# age = int(input("Enter your age: \n"))
+
+# if age >= 18:
+#     print("You are an adult. Your ticket costs 100 dollars")
+# else:
+#     print("You are a child. Your ticket costs 50 dollars")
+    
+
+
+
+
+
+
+
+
+height = int(input("Enter your height in cm: \n"))
+age = int(input("Enter your age: \n"))
+
+if height >= 110 and age >= 10:
+    print("You can go inside")
+else:
+    print("You cannot go inside")
+
---
+++ Lesson_5/main.py
@@ -0,0 +1,13 @@
+# colors = ["red", "green", "blue", "yellow"]
+
+# for x in colors:
+#     print(x)
+# else:
+#     print("Done")
+
+import time
+
+for i in range(5):
+    print("Spaceship is ready to launch")
+    time.sleep(1)
+    
